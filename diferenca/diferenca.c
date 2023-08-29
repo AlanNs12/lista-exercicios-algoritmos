9cc0022bf798a5ecdb4a20a7536bcc59db36f8e7
Alan Nascimento <alannascimento.contato@gmail.com>
2023-08-29 20:57:04 -0300

    initial-commit

---
+++ diferenca/diferenca.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A;
+    int B;
+    int C;
+    int D;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+    
+    printf("DIFERENCA = %d\n", (A*B-C*D));
+ 
+    return 0;
+}
---
+++ produto-simples/produto-simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int valor1;
+    int valor2;
+    int PROD;
+    
+    scanf("%d", &valor1);
+    scanf("%d", &valor2);
+    
+    printf("PROD = %d\n", valor1*valor2);
+    
+ 
+    return 0;
+}
